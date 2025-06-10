7ae0e2232c56e0861757eba5ef120ff43de38893
souvikjs01 <souvik741156@gmail.com>
2025-06-10 19:31:48 +0530

    recent invoices

--- app/dashboard/invoices/_components/InvoiceList.tsx
+++ app/dashboard/invoices/_components/InvoiceList.tsx
@@ -1,10 +1,18 @@
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { 
+    Table, 
+    TableBody, 
+    TableCell, 
+    TableHead, 
+    TableHeader, 
+    TableRow 
+} from '@/components/ui/table'
 import React from 'react'
 import InvoiceActions from './InvoiceActions'
 import { prisma } from '@/lib/prisma'
 import { requireUser } from '@/lib/hook';
 import { formatCurrency } from '@/app/utils/formatCurrency';
 import { Badge } from '@/components/ui/badge';
+import EmptyState from '@/components/dashboard/EmptyState';
 
 
 async function getData(userId: string) {
@@ -32,47 +40,53 @@ export default async function InvoiceList() {
   const session = await requireUser();
   const data = await getData(session.user?.id as string)
   return (
-    <Table>
-        <TableHeader>
+    <>
+      {data.length === 0 ? (
+        <EmptyState
+          title="No invoices found"
+          description="Create an invoice to get started"
+          buttontext="Create invoice"
+          href="/dashboard/invoices/create"
+        />
+      ) : (
+        <Table>
+          <TableHeader>
             <TableRow>
-                <TableHead>Invoice ID</TableHead>
-                <TableHead>Customer</TableHead>
-                <TableHead>Amount</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Date</TableHead>
-                <TableHead className=' text-right'>Actions</TableHead>
+              <TableHead>Invoice ID</TableHead>
+              <TableHead>Customer</TableHead>
+              <TableHead>Amount</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
             </TableRow>
-        </TableHeader>
-
-        <TableBody>
+          </TableHeader>
+          <TableBody>
             {data.map((invoice) => (
-                <TableRow key={invoice.id}>
-                    <TableCell>
-                        #{invoice.invoiceNumber}
-                    </TableCell>
-                    <TableCell>
-                        {invoice.clientName}
-                    </TableCell>
-                    <TableCell>
-                        {formatCurrency({
-                            amount: invoice.total,
-                            currency: invoice.currency as any,
-                        })}
-                    </TableCell>
-                    <TableCell>
-                        <Badge>{invoice.status}</Badge>
-                    </TableCell>
-                    <TableCell>
-                        {new Intl.DateTimeFormat("en-US", {
-                            dateStyle: "medium",
-                        }).format(invoice.createdAt)}
-                    </TableCell>
-                    <TableCell className=' text-right'>
-                        <InvoiceActions id={invoice.id} status={invoice.status}/>
-                    </TableCell>
-                </TableRow>
+              <TableRow key={invoice.id}>
+                <TableCell>#{invoice.invoiceNumber}</TableCell>
+                <TableCell>{invoice.clientName}</TableCell>
+                <TableCell>
+                  {formatCurrency({
+                    amount: invoice.total,
+                    currency: invoice.currency as any,
+                  })}
+                </TableCell>
+                <TableCell>
+                  <Badge>{invoice.status}</Badge>
+                </TableCell>
+                <TableCell>
+                  {new Intl.DateTimeFormat("en-US", {
+                    dateStyle: "medium",
+                  }).format(invoice.createdAt)}
+                </TableCell>
+                <TableCell className="text-right">
+                  <InvoiceActions status={invoice.status} id={invoice.id} />
+                </TableCell>
+              </TableRow>
             ))}
-        </TableBody>
-    </Table>
+          </TableBody>
+        </Table>
+      )}
+    </>
   )
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,46 @@
 import { DashboardBlocks } from '@/components/dashboard/DashboardBlocks';
+import EmptyState from '@/components/dashboard/EmptyState';
 import { InvoiceGraph } from '@/components/dashboard/InvoiceGraph';
+import RecentInvoices from '@/components/dashboard/RecentInvoices';
+import { Skeleton } from '@/components/ui/skeleton';
 import { requireUser } from '@/lib/hook'
-import React from 'react'
+import { prisma } from '@/lib/prisma';
+import React, { Suspense } from 'react'
+
+async function getData(userId: string) {
+  const data = await prisma.invoice.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return data;
+}
 
 export default async function DashboardRoute() {
   const session = await requireUser();
+  const data = await getData(session.user?.id as string)
   return (
-    <div>
-      <DashboardBlocks />
-      <div className="grid gap-4 lg:grid-cols-3 md:gap-8">
-        <InvoiceGraph />
-      </div>
-    </div>
+    <>
+      {data.length < 1 ? (
+        <EmptyState
+          title="No invoices found"
+          description="Create an invoice to see it right here"
+          buttontext="Create Invoice"
+          href="/dashboard/invoices/create"
+        />
+      ) : (
+        <Suspense fallback={<Skeleton className="w-full h-full flex-1" />}>
+          <DashboardBlocks />
+          <div className="grid gap-4 lg:grid-cols-3 md:gap-8">
+            <InvoiceGraph />
+            <RecentInvoices />
+          </div>
+        </Suspense>
+      )}
+    </>
   )
 }
